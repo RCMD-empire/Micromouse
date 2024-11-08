e4be1cfad91abc3396dda5c8695ac56877dbf33f
RCMD-LaptopB <rcmd>
2024-11-08 11:13:13 +0100

    IRSensors def

--- include/components.h
+++ include/components.h
@@ -2,13 +2,17 @@
 // Micromouse course - evosoft
 #pragma once
 
+#include <irsensor.h>
 #include "hal/micromouse.h"
 #include "variables.h"
 
 namespace MM {
 
 struct Components {
-
+IRSensor ir_left {IR_2, IR_EN_2, vars.ir_left_raw, vars.ir_en_left};
+IRSensor ir_frontleft {IR_1, IR_EN_1, vars.ir_frontleft_raw, vars.ir_en_frontleft};
+IRSensor ir_right {IR_3, IR_EN_3, vars.ir_right_raw, vars.ir_en_right};
+IRSensor ir_frontleft {IR_4, IR_EN_4, vars.ir_frontright_raw, vars.ir_en_frontright};
 };
 
 } // namespace MM
